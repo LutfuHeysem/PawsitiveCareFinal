f170688d86e12c76f9ea52d9350f0b1d75cf30c6
IlginVural <vuralilgin05@gmail.com>
2024-05-11 12:39:21 +0300

    ClickListener
    Implemented the clicklisteners to navigate through pages inside home page

--- PawSitive2/app/src/main/java/com/example/pawsitive/HomePage.java
+++ PawSitive2/app/src/main/java/com/example/pawsitive/HomePage.java
@@ -1,25 +1,106 @@
 package com.example.pawsitive;
 
+import static com.example.pawsitive.R.id.homeIcon;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity {
 
+    private ArrayList<User> usersOnDisplay;
+
+
+    public HomePage(){
+        this.usersOnDisplay = new ArrayList<>();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
+
+        initializeImageViews();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+
+        }
+    private void initializeImageViews(){
+        ImageView homeIcon = findViewById(R.id.home_icon);
+        ImageView favourtiesIcon = findViewById(R.id.heart_icon);
+        ImageView addIcon = findViewById(R.id.add_icon);
+        ImageView chatIcon = findViewById(R.id.chat_icon);
+        ImageView profileIcon = findViewById(R.id.profile_icon);
+
+        homeIcon.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //handle click on home image
+                //navigate to home page
+                Intent intent = new Intent(HomePage.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
+
+        favourtiesIcon.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //handle click on favourites image
+                //navigate to favourite page
+                Intent intent = new Intent(HomePage.this, FavouritesPage.class);
+                startActivity(intent);
+            }
+        });
+
+        addIcon.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //handle click on add image
+                //navigate to add/edit page
+                Intent intent = new Intent(HomePage.this, AddEditPet.class);
+                startActivity(intent);
+            }
+        });
+
+        chatIcon.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //handle click on chat image
+                //navigate to chat page
+                Intent intent = new Intent(HomePage.this, ChatActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        profileIcon.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //handle click on profile image
+                //navigate to profile page
+                Intent intent = new Intent(HomePage.this, ProfilePage1.class);
+                startActivity(intent);
+            }
+        });
+    };
+
 }
