c7a02fc96079eb380cd683cf6906065b2f6462f0
Lütfü Heysem <heysem1104@gmail.com>
2024-05-12 18:56:01 +0300

    Login update

--- PawSitive2/app/src/main/java/com/example/pawsitive/acitvities/Login.java
+++ PawSitive2/app/src/main/java/com/example/pawsitive/acitvities/Login.java
@@ -90,12 +90,9 @@ public class Login extends AppCompatActivity {
                                         Intent chatIntent = new Intent(Login.this, UsersActivity.class);
                                         Intent reviewIntent = new Intent(Login.this, ReviewMain.class);
                                         Intent profileIntent = new Intent(Login.this, ProfilePage1.class);
-<<<<<<< Updated upstream
-                                        startActivity(reviewIntent);
-=======
-
+                                        
                                         startActivity(homeIntent);
->>>>>>> Stashed changes
+
                                     }
                                     else if(currUser != null && !currUser.isEmailVerified())
                                         Toast.makeText(Login.this, "Verify your email first!", Toast.LENGTH_SHORT).show();
